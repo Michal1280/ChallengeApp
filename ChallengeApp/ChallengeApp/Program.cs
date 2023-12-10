086ba86ddb8881245c8ebb91384b6d4f6db5139a
Michal <michal.zebrowski.it@gmail.com>
2023-12-10 18:28:32 +0100

    Variables

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Integer numbers variable.
+//var myAge = 33;
+int myAge = 120;
+int newAge = myAge + 5;
+int myVar = int.MinValue;
+uint myVar2  = uint.MaxValue;
+long myVar3 = long.MaxValue;
+
+
+// Decimal numbers, float it is enough for most of the cases. 
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+decimal myNumber3 = decimal.MaxValue;
+
+//String Variable
+string name = "Michal";
+string surname = "Zebrowski";
+string result = name + surname + myAge;
+//Console.WriteLine(result);
+
+//Sign variable
+char myVar4 = '$';
+var result2 = name.ToArray();
+
+//Logical variable
+bool isActive  = true;
+isActive = false;
+var isValid = 5 > 6;
+Console.WriteLine(isValid);
