bad856242dce69e36401418c864a521dbf8be89a
Michal <michal.zebrowski.it@gmail.com>
2024-01-01 18:00:14 +0100

    Exercise 14

--- ChallengeApp/ChallengeApp/Person.cs
+++ ChallengeApp/ChallengeApp/Person.cs
@@ -1,15 +1,18 @@
 ﻿namespace ChallengeApp
-    public abstract class Person //:System.Object
+
 {
-    public Person(string name, string surname, char sex)
+    public abstract class Person
     {
-        this.Name = name;
-        this.Surname = name;
-        this.Sex = sex;
+        public Person(string name, string surname, char sex)
+        {
+            this.Name = name;
+            this.Surname = name;
+            this.Sex = sex;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public char Sex { get; private set; }
     }
 
-    public string Name { get; private set; }
-    public string Surname { get; private set; }
-    public char Sex { get; private set; }
-}
 }
